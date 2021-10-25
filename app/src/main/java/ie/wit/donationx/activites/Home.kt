e935d2427341a5d7b6aa01062d3ccd121b81e08a
Ian <ianhutch90@gmail.com>
2021-10-25 19:40:15 +0100

    Add container to host nav graph (NavHostFragment)

---
+++ app/src/main/java/ie/wit/donationx/activites/Home.kt
@@ -0,0 +1,12 @@
+package ie.wit.donationx.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ie.wit.donationx.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
