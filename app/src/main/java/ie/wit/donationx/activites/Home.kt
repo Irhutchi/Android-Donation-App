abece0569153c19fd0e131d5a1f9b5ab940c3107
Ian <ianhutch90@gmail.com>
2021-10-25 21:27:57 +0100

    Add Nav Drawer
    Introduced a drawerLayout and a navigationView component

--- app/src/main/java/ie/wit/donationx/activites/Home.kt
+++ app/src/main/java/ie/wit/donationx/activites/Home.kt
@@ -2,11 +2,35 @@ package ie.wit.donationx.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.*
+import androidx.navigation.ui.*
 import ie.wit.donationx.R
+import ie.wit.donationx.databinding.HomeBinding
 
 class Home : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding : HomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
+
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
